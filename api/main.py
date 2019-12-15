bc77ef103f052732002f17526afd777f16a49234
pieceofeden <ashishranjan079756@gmail.com>
2019-12-15 14:04:40 +0530

    Added feature for uploading images

--- api/main.py
+++ api/main.py
@@ -1,5 +1,32 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os
 app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#Homepage of the webapp
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#upload part 
+@app.route('/upload', methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.makedirs(target)
+
+    for file in request.files.getlist('file'):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template('upload_completed.html')
+
 
 # TODO: Complete this method
 # Input: Uploaded image as request body parameter
